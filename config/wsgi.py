55a10ac50cd07b07b7ba8ea44796f2a6e1d6850c
Murodjon22 <bozorovmurodjon70@gmail.com>
2025-06-02 11:23:40 +0200

    news

--- config/wsgi.py
+++ config/wsgi.py
@@ -15,4 +15,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+# BASE_DIR ni o'zingiz aniqlashingiz kerak:
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
